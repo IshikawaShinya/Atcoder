aacc77bf88691e8f51c3edd522d32866de049cb5
IshikawaShinya <l.j.6.v.v.c@gmail.com>
2024-06-03 19:23:15 +0900

    a

--- abc356/a.py
+++ abc356/a.py
@@ -1,14 +1,18 @@
 def subsegmentReverse():
     N, L, R = map(int,input().split())
-    list =[]
-    output = ""
-    for i in range(N):
-        if i >= L - 1 and i <= R - 1:
-            output = output + "$R + L -i - 1"
-            list.append(R + L -i - 1)
-            continue
-        list.append(i+1)
-    print(" ".join(map(str,list)))
+    #解答１
+    # list =[]
+    # for i in range(N):
+    #     if i >= L - 1 and i <= R - 1:
+    #         list.append(R + L -i - 1)
+    #         continue
+    #     list.append(i+1)
+    # print(" ".join(map(str,list)))
+
+    #解答2
+    # A = list(range(1, N + 1))
+    # A[L - 1 : R] = A[L - 1 : R][::-1]
+    # print(*A)
     return 
 
 subsegmentReverse()
