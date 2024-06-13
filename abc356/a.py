a00603fa15aa13e9e5c7652c7acd5c8ca28ce358
IshikawaShinya <l.j.6.v.v.c@gmail.com>
2024-06-14 01:36:57 +0900

    remove node_modules

--- abc356/a.py
+++ abc356/a.py
@@ -1,13 +1,13 @@
 def subsegmentReverse():
     N, L, R = map(int,input().split())
     #解答１
-    # list =[]
-    # for i in range(N):
-    #     if i >= L - 1 and i <= R - 1:
-    #         list.append(R + L -i - 1)
-    #         continue
-    #     list.append(i+1)
-    # print(" ".join(map(str,list)))
+    list =[]
+    for i in range(N):
+        if i >= L - 1 and i <= R - 1:
+            list.append(R + L -i - 1)
+            continue
+        list.append(i+1)
+    print(" ".join(map(str,list)))
 
     #解答2
     # A = list(range(1, N + 1))
--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-// 標準入力を取得する
-var inputs = fs.readFileSync("/dev/stdin", "utf8");
-var inputArray = inputs.split(/\s/);
-//アウトプット
-console.log("input " + inputArray[0] + " " + inputArray[1] + " " + inputArray[2]);
